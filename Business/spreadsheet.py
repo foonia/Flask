377f64e9d89913692cda0e0338d76fe12ed01085
sechanchoi <sechan94@naver.com>
2021-07-14 15:45:34 +0900

    [DEL] spreadsheet.py for test

--- Business/spreadsheet.py
+++
@@ -1,8 +0,0 @@
-import sys,os
-sys.path.append(os.path.dirname(os.path.abspath((os.path.dirname(__file__)))))
-from globalpackage import lib
-from dataflow import *
-
-u = updateData()
-u.runupdate()
-
--- globalpackage/lib.py
+++ globalpackage/lib.py
@@ -1,13 +1,10 @@
+import sys,os
+sys.path.append(os.path.dirname(os.path.abspath((os.path.dirname(__file__)))))
 from gspread_dataframe import set_with_dataframe,get_as_dataframe
 import pandas as pd
 import gspread as gd
 
 
-
-import sys,os
-sys.path.append(os.path.dirname(os.path.abspath((os.path.dirname(__file__)))))
-
-from globalpackage import config
 from Business import setting
 from Business import readandwrite
 from Business import logic
